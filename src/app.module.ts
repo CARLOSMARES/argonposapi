c6dae80a400f8e10e4311fb5c0391f28b9ac8532
CARLOSMARES <microsystemags@hotmail.com>
2025-07-21 01:44:27 -0600

    feat: update dependencies and add configuration module
    - Added @nestjs/config, @nestjs/typeorm, and mysql2 to dependencies.
    - Removed AppController, AppService, and their corresponding tests.
    - Updated AppModule to include ConfigModule for environment variable management.
    - Created .env.example file for environment variable configuration.
    - Added User entity skeleton in src/entities/user.entity.ts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+@Module(
+  {
+    imports: [
+      ConfigModule.forRoot(
+        {
+          isGlobal: true,
+          envFilePath: ['.env', '.env.local'],
+        }
+      )
+    ],
+    controllers: [
+      AppController
+    ],
+    providers: [
+      AppService
+    ],
+  }
+)
+export class AppModule { }
